8a7945e0f98f9ddaa46ef61cbd227872a46551e4
Megan Mirnalini Sundaram R <mrnalini@live.in>
2025-12-13 17:27:06 +0100

    Adding updated code (for paths and references) for merged dataset

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=mask2former
 #SBATCH --time=12:00:00
 #SBATCH --partition=gpu-2080ti-11g
-#SBATCH --output=/home/mrajaraman/slurm/mask2former/train/output-%A.out
+#SBATCH --output=/home/mrajaraman/slurm/mask2former/merged-train/output-%A.out
 #SBATCH --gres=gpu:1
 
 echo "## Starting GPU test on $HOSTNAME"
@@ -58,3 +58,12 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 # # MODEL.WEIGHTS /h/jquinto/Mask2Former/model_final_3c8ec9.pkl \
 
 # ---- TRAINING COMMANDS FOR MERGED DATASET BELOW ----
+python train_net_merged.py --num-gpus 1 \
+--exp_id ${TILE_SIZE} \
+--train_iter 25000 \
+--config-file /home/mrajaraman/master-thesis-dragonfly/external/mask2former-dragonfly/configs/lifeplan/instance-segmentation/maskformer2_R50_bs16_50ep.yaml \
+--dataset_path /home/mrajaraman/dataset/dataset-v2-coco/ \
+OUTPUT_DIR output_${TILE_SIZE}_dragonfly_${TIMESTAMP} \
+DATASETS.TRAIN "(\"dragonfly_merged_train\",)" \
+DATASETS.TEST "(\"dragonfly_merged_valid\",)"  \
+# MODEL.WEIGHTS /h/jquinto/Mask2Former/model_final_3c8ec9.pkl \
